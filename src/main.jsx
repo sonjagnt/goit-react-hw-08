d57b90a4faf5de60c16b19e1e9709d9831c14a61
sonja <signtieva@gmail.com>
2025-02-01 15:40:14 +0200

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,11 @@ import { PersistGate } from "redux-persist/integration/react";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
